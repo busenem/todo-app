aae312c49dea865fee11325ba3bd7c96f459273f
Buse Senem Ipekel <buse.ipekel@hititcs.com>
2025-07-11 13:38:46 +0300

    Entity paketine Todo sınıfı taşındı

--- todo-app/src/main/java/com/example/todo/service/TodoService.java
+++ todo-app/src/main/java/com/example/todo/service/TodoService.java
@@ -1,6 +1,6 @@
 package com.example.todo.service;
 
-import com.example.todo.Todo;
+import com.example.todo.entity.Todo;
 import com.example.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
